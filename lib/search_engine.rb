a464db739a1553fef4f2c57e6c20b7235bd55304
James Coglan <jcoglan@googlemail.com>
2009-08-07 16:23:48 +0100

    Implementing the word frequency and location scoring routines for the text search engine.

--- lib/search_engine.rb
+++ lib/search_engine.rb
@@ -6,6 +6,10 @@ require 'hpricot'
 module SearchEngine
   IGNORE_WORDS = %w[the of to and a in is it]
   
+  def self.db
+    ActiveRecord::Base.connection
+  end
+  
   def self.crawl(pages, depth = 2)
     (0...depth).each do |i|
       newpages = Set.new
@@ -87,24 +91,85 @@ module SearchEngine
     # More efficient than book version: get
     # all the word IDs with one query
     entries = Word.find(:all, :conditions => ["word in (#{ words.map{'?'} * ',' })"] + words)
-    ids = entries.map(&:id).each_with_index
+    word_ids = entries.map(&:id)
     range = 0...(entries.size)
     
     query = <<-EOQ
       SELECT w0.page_id, #{ range.map { |i| "w#{i}.location" } * ', ' }
       FROM   #{ range.map { |i| "#{WordLocation.table_name} w#{i}" } * ', ' }
       WHERE  #{ ( range.each_cons(2).map { |a,b| "w#{a}.page_id = w#{b}.page_id" } +
-                  ids.map { |id,i| "w#{i}.word_id = #{id}" } ) *
+                  word_ids.each_with_index.map { |id,i| "w#{i}.word_id = #{id}" } ) *
                 "\n      AND    " }
     EOQ
     
-    db.execute(query).map do |result|
+    rows = db.execute(query).map do |result|
       [result[0].to_i] + range.map { |i| result[i+1].to_i }
+    end
+    [rows, word_ids]
+  end
+  
+  def self.get_scored_list(rows, word_ids)
+    total_scores = rows.inject({}) do |table, row|
+      table[row[0]] = 0
+      table
+    end
+    
+    weights = [ [1.0,frequency_score(rows)],
+                [1.5,location_score(rows)] ]
+    
+    weights.each do |(weight, scores)|
+      total_scores.each_key do |url|
+        total_scores[url] += weight * scores[url]
+      end
+    end
+    
+    total_scores
+  end
+  
+  def self.frequency_score(rows)
+    counts = rows.inject({}) { |table,row| table[row[0]] = 0; table }
+    rows.each { |row| counts[row[0]] += 1 }
+    normalize_scores(counts)
+  end
+  
+  def self.location_score(rows)
+    locations = rows.inject({}) { |table,row| table[row[0]] = 1_000_000; table }
+    rows.each do |row|
+      loc = row[1..-1].inject(0) { |a,b| a + b }
+      locations[row[0]] = loc if loc < locations[row[0]]
+    end
+    normalize_scores(locations, true)
+  end
+  
+  def self.get_url_name(id)
+    Page.find(id).url
+  end
+  
+  def self.query(q)
+    rows, word_ids = *get_match_rows(q)
+    scores = get_scored_list(rows, word_ids)
+    ranked_scores = scores.map { |id,score| [score,id] }.sort_by { |r| r[0] }.reverse
+    ranked_scores[0...10].each do |(score,id)|
+      puts "%f\t%s" % [score, get_url_name(id)]
     end
   end
   
-  def self.db
-    ActiveRecord::Base.connection
+  def self.normalize_scores(scores, small_is_better = false)
+    vsmall = 0.00001 # Avoid division by zero
+    if small_is_better
+      minscore = scores.values.min
+      scores.inject({}) do |table,(u,l)|
+        table[u] = minscore.to_f / [vsmall,l].max
+        table
+      end
+    else
+      maxscore = scores.values.max
+      maxscore = vsmall if maxscore.zero?
+      scores.inject({}) do |table,(u,c)|
+        table[u] = c.to_f / maxscore
+        table
+      end
+    end
   end
   
 end
